7356c5f9eca29947eaeced53a85829b895a6ecf1
Andre Fernandes Cristofolini Melo <andre.melo@dotgroup.com.br>
2022-03-09 22:15:49 -0300

    Fix: add prevent default to menu mobile event, avoiding open and close at same time

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,6 +1,6 @@
 import outsideClick from './outsideclick.js';
 
-export default class MMenuMobile {
+export default class MenuMobile {
   constructor(menuButton, menuList, userEvents = ['click', 'touchstart']) {
     this.menuButton = document.querySelector(menuButton);
     this.menuList = document.querySelector(menuList);
@@ -8,9 +8,10 @@ export default class MMenuMobile {
     this.openMenu = this.openMenu.bind(this);
   }
 
-  openMenu() {
-    this.menuList.classList.add('is-active');
-    this.menuButton.classList.add('is-active');
+  openMenu(event) {
+    event.preventDefault();
+    this.menuList.classList.toggle('is-active');
+    this.menuButton.classList.toggle('is-active');
 
     outsideClick(this.menuList, this.userEvents, () => {
       this.menuList.classList.remove('is-active');
